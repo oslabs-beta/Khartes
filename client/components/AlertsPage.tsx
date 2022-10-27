e00f949b0d8508f46dca4b5514fb89d3438cfbfb
Joe Heffernan <joeheffernan@Joes-MacBook-Air.local>
2022-10-26 17:29:53 -0700

    working on styling

--- client/components/AlertsPage.tsx
+++ client/components/AlertsPage.tsx
@@ -1,17 +1,31 @@
 import * as React from 'react';
 
+import ProblemObject from './problemobject';
+
 const AlertsPage = () => {
 
     // this component will display graphs, issue descript and two buttons auto-fix or fix options
     // buttons may need to be updated with new content based on our shifting MVP
         return(
           <div>
+            <div id="contents">
+              <div id="list">
+                <ProblemObject />
+                <ProblemObject />
+                <ProblemObject />
+                <ProblemObject />
+                <ProblemObject />
+                <ProblemObject />
+                  <ProblemObject />
+                <ProblemObject />
+                <ProblemObject />
+                <ProblemObject />
+                <ProblemObject />
+                <ProblemObject />
+              </div>
             {/* <NavBar />
            <Chart /> */}
-           <div> Description of Issues </div>
-           <button> Auto-fix </button>
-           <button> Fix options </button>
-            
+           </div>
           </div>
         )
       }
