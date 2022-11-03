3f430212e294dd2924d1ab62d3b6ab991912c287
Tony Diethelm <tonydiethelm@gmail.com>
2022-11-03 12:08:06 -0700

    Write should add a new alert objec to the DB

--- server/Controllers/generateTests.js
+++ server/Controllers/generateTests.js
@@ -12,42 +12,48 @@ async function makeExamples(){
     console.log('Make some test cases in the DB');
 
     const testCases = [{
+        "id": 000000001,
         "issue": "Pontential OOMKill",
         "status": "new",
         "node": "gke-khartes-default-pool-051f007c-mt19",
         "pod": "deployment-memoryuser-564878964b-pfctt",
         "container": "",
-        "metrics": "200000",
+        "metrics": 200000,
+        "limit": 200000,
         "historicalMetrics": [],
         "oldYaml": {},
         "newYaml": {},
       },
       
       {
+        "id": 000000002,
         "issue": "Pontential Persistant Volume Disk Full",
         "status": "new",
         "node": "gke-khartes-default-pool-051f007c-jw75",
         "pod": "deployment-polo-67cd6dd5cf-54vbd",
         "container": "",
-        "metrics": "15000",
+        "metrics": 15000,
+        "limit": 15000,
         "historicalMetrics": [],
         "oldYaml": {},
         "newYaml": {},
       },
       
       {
+        "id": 000000003,
         "issue": "Potential Node Burst config",
         "status": "new",
         "node": "gke-khartes-default-pool-051f007c-mt19",
         "pod": "deployment-memoryuser-564878964b-pfctt",
         "container": "",
-        "metrics": "200000",
+        "metrics": 200000,
+        "limit": 200000,
         "historicalMetrics": [],
         "oldYaml": {},
         "newYaml": {},
       }]
 
-    fs.writeFileSync(path.join(__dirname, '../../server/db.json'), JSON.stringify(testCases));
+    await fs.writeFileSync(path.join(__dirname, '../../server/db.json'), JSON.stringify(testCases));
 }
 
 makeExamples();
