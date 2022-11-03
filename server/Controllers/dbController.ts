577037edcdfa5a271e02268f0dcd9f98b1033a28
Tony Diethelm <tonydiethelm@gmail.com>
2022-11-03 14:54:44 -0700

    Further progress on controllers, fake historical data added to fake alerts.

--- server/Controllers/dbController.ts
+++ server/Controllers/dbController.ts
@@ -4,7 +4,7 @@ This is going to use node fs.
 Read
 Write       overwrites by default. Do we want that? No. Need to append, but doesn't work
 exists      takes in baby object (pod, issue) and checks against already existing alert objects in the DB.
-delete      delets alert object. 
+delete      deletes alert object. 
 update      given an ID and a status, update the alert object. 
 */
 
@@ -45,18 +45,37 @@ export const dbController = {
         
         //get new alert, db as text, and db as array of alert objects. 
         const newAlert = request.body;                                                                      //Might need to change this in future?
-        const dbAsText = await fs.readFileSync(path.join(__dirname, '../../../server/db.json'), 'utf8')
+        const dbAsText:string = await fs.readFileSync(path.join(__dirname, '../../../server/db.json'), 'utf8')
         const dbAsArray = JSON.parse(dbAsText);
         
         //push new alert onto array of alert objects.
         dbAsArray.push(newAlert);
         
         //write it all back to the DB.
-        fs.writeFileSync(path.join(__dirname, '../../server/db.json'), JSON.stringify(dbAsArray));
+        await fs.writeFileSync(path.join(__dirname, '../../server/db.json'), JSON.stringify(dbAsArray));
         
         return next();
     },
 
+    //Need to update an alert status.
+    //ID and new status should come in as parameters. 
+    //Will need to read it, json parse it to an array of objects, change it...
+    //Then restringify and write it. 
+    write: async (request: Request, response: Response, next: NextFunction) => {
+        
+        //get new alert, db as text, and db as array of alert objects. 
+        const newAlert = request.body;                                                                      //Might need to change this in future?
+        const dbAsText:string = await fs.readFileSync(path.join(__dirname, '../../../server/db.json'), 'utf8')
+        const dbAsArray = JSON.parse(dbAsText);
+        
+        //push new alert onto array of alert objects.
+        dbAsArray.push(newAlert);
+        
+        //write it all back to the DB.
+        await fs.writeFileSync(path.join(__dirname, '../../server/db.json'), JSON.stringify(dbAsArray));
+        
+        return next();
+    },
     
 };
 
