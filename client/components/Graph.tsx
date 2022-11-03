49b0bc0a6ff734db520f66cb6c83122f42cbb156
Mark Nichting <mtnichting@gmail.com>
2022-11-03 12:44:19 -0700

    line chart rendering with fake data

---
+++ client/components/Graph.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+
+import { Line } from 'react-chartjs-2';
+import { Chart, CategoryScale, LinearScale, PointElement, LineElement } from "chart.js";
+
+Chart.register(CategoryScale, LinearScale, PointElement, LineElement);
+const data = {
+  labels: [15, 30, 45, 60, 75],
+  datasets: [{
+    label: 'pod abc',
+    data: [1, 2, 3, 4, 5]
+  }]
+}
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+
+const Graph = () => (
+  <div>
+    <h1>Line Chart</h1>
+    <Line options={options}  data={data} />
+  </div>
+  // <Line/>
+)
+
+
+export default Graph;
+
+
+
+
+// import React from 'react';
+// import {
+//   Chart as ChartJS,
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+//   LineElement,
+//   Title,
+//   Tooltip,
+//   Legend,
+// } from 'chart.js';
+// import { Line } from 'react-chartjs-2';
+// import faker from 'faker';
+
+// ChartJS.register(
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+//   LineElement,
+//   Title,
+//   Tooltip,
+//   Legend
+// );
+
+// export const options = {
+//   responsive: true,
+//   plugins: {
+//     legend: {
+//       position: 'top' as const,
+//     },
+//     title: {
+//       display: true,
+//       text: 'Chart.js Line Chart',
+//     },
+//   },
+// };
+
+// const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+// export const data = {
+//   labels,
+//   datasets: [
+//     {
+//       label: 'Dataset 1',
+//       data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+//       borderColor: 'rgb(255, 99, 132)',
+//       backgroundColor: 'rgba(255, 99, 132, 0.5)',
+//     },
+//     {
+//       label: 'Dataset 2',
+//       data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+//       borderColor: 'rgb(53, 162, 235)',
+//       backgroundColor: 'rgba(53, 162, 235, 0.5)',
+//     },
+//   ],
+// };
+
+// export function App() {
+//   return <Line options={options} data={data} />;
+// }
