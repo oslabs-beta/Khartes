55e30b180050209cffed8ea1e66048a088005387
Tony Diethelm <tonydiethelm@gmail.com>
2022-11-05 10:02:36 -0700

    Saving before I switch to upload a new DB.json file

---
+++ server/Controllers/fixTheYaml.ts
@@ -0,0 +1,6 @@
+/* What I need this to do...
+Take in the alert object (in the body) and the percentage that the client wants the limits bumped by (in the params). 
+Put id on params.id and put newyaml on params.newYaml.
+Will then go to dbController.updateYamlById
+
+*/
