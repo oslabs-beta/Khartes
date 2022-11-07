d6312b1a6cd5eeadf3ad54d59f880529cc5a845c
Tony Diethelm <tonydiethelm@gmail.com>
2022-11-07 11:27:08 -0800

    fixTheYaml should work, in theory.

--- server/Controllers/fixTheYaml.ts
+++ server/Controllers/fixTheYaml.ts
@@ -1,6 +1,6 @@
 /* What I need this to do...
 Take in the alert object (in the body) and the percentage that the client wants the limits bumped by (in the params). 
-
+increase memory limits by percentage.
 
 Will then go to dbController.updateYamlById
     Needs params.id, params.newYaml as text
@@ -10,10 +10,10 @@ Will then go to dbController.updateYamlById
 //setup stuff
 import { Request, Response, NextFunction } from 'express';
 import path from 'path';
-import YAML from ('yaml');
+const YAML = require('yaml');
 
 //make the new yaml
-export const makeNewYaml: async (request: Request, response: Response, next: NextFunction) => {
+export function makeNewYaml(request: Request, response: Response, next: NextFunction): void {
     
     //get everything I need to make the new yaml
     //id            off body.id?
@@ -21,25 +21,24 @@ export const makeNewYaml: async (request: Request, response: Response, next: Nex
     //oldYaml       off body.oldyaml?
     //newYaml       parsed oldYaml
     
-    const percentageToFixBy:number = parseInt(request.params.percentage)
-    const oldYaml:string = request.body.oldYaml;
+    const percentageToFixBy:any = request.params.percentage;
+    const oldYaml:any = request.body.oldYaml;
     let newYaml:any = YAML.parse(oldYaml);
+    let limit:any = newYaml[0].resources.limits.memory;
 
     // I want to change yaml[0].resources.limits.memory. 
     // but it's text. "200M". How to parse? Separate numbers and letters, change numbers, restringify, add back together. 
-    // const test = "200M";
-
-    // const numbers = Number(test.replace(/\D/g, ''));
-    // console.log(numbers);
 
     
-    let numberPartOfString:string = 
-    let stringPartOfString:string = 
+    let numberPartOfString:number = Number(limit.replace(/\D/g, ''));  //regex, all NON digit characters erased.
+    let stringPartOfString:string = limit.replace(/\D/g, '');  //regex, all DIGIT characters erased.
 
-    newYaml[0].resources.limits.memory *= (1 + (percentageToFixBy / 100));
+    numberPartOfString *= (1 + (percentageToFixBy / 100));
+
+    newYaml[0].resources.limits.memory = numberPartOfString.toString() + stringPartOfString;
 
     //Now off to dbController.updateYamlById. 
-    request.params.id = parseInt(request.body.id);  //why is params.id a string? Look into request interface.
+    request.params.id = request.body.id;  //why is params.id a string? Look into request interface.
     request.params.newYaml = newYaml;
 
     return next();
