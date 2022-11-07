77a5a0d825381dc3e2b489b76c814359364a6b38
JP Keith <jpkeith15@gmail.com>
2022-11-07 14:57:08 -0800

    fixing TS to use ES modules

--- server/heartbeat.ts
+++ server/heartbeat.ts
@@ -1,4 +1,4 @@
-import getPods from "./Controllers/getPods"; // calling this should return an array of yamls for each pod
+import getPods from "./Controllers/getPods";
 import getPrometheusData from "./Controllers/getPrometheusData";
 import {createAlert} from "./Controllers/createAlert";
 import checkForOomkill from "./Controllers/checkForOomkill";
@@ -13,40 +13,23 @@ const nodeBurstIssue = "Potential Node Burst issue detected"
 
 /*
 Pull data every X(15) seconds, calls the correct controllers.
-    We have a pod name.
-    get memory used
-    get memory limits
-    get HD used
-    get HD limits
-
+    
     
 check the data
     if problem detected, create baby alert and check it against already created alerts in the db. 
-        dbController.exists?
+        dbController.exists
     create Alert object if problem detected.
-            issue text
-            status: new
-            container:
-            node:
-            pod:
-            used
-        limit
-         get historical data   1 hour. 
-            get old yaml
-            new yaml: empty    
-
-
-write to DB
+           
 */
 
-
+const startHeartbeat = () => {
 //Start port-forwarding
-startPortForward();
-
+  startPortForward();
+  console.log("we're in the heartbeat function babump")
 
 
 //call getPods to get the list of pods and their associated nodes in an object.
-const podsList = getPods();
+  const podsList = getPods();
 
 
 // getPrometheusData(pod, node)
@@ -61,26 +44,28 @@ const podsList = getPods();
 //send this data point to checkForNodeBurst. If it returns true. We need to build an alert with createAlert.
 
 
+//check request vs limit for overallocated resources if we want to do Over allocated resources?
 
 
 //iterate through this list and plug the associated pod and node into getPrometheusData (send a complete promQL query) Specifically for OomKill
 //we will receive back the data point from prometheus
 //send this data point to checkForOomkill. If it returns true. We need to build an alert with createAlert.
-for(let i = 0; i < podsList.length; i++){
-    //OOMKILL
-    const memUsageQuery = 'container_memory_usage_bytes';
-    const memUsage:any = getPrometheusData(podsList[i].pod, 'container_memory_usage_bytes');
-    const memLimit:any = getPrometheusData(podsList[i].pod, 'container_spec_memory_limit_bytes');
-    const oomkill = checkForOomkill(memUsage, memLimit);
+  for(let i = 0; i < podsList.length; i++){
+      //OOMKILL
+      const memUsageQuery = 'container_memory_usage_bytes';
+      const memUsage:any = getPrometheusData(podsList[i].pod, 'container_memory_usage_bytes');
+      const memLimit:any = getPrometheusData(podsList[i].pod, 'container_spec_memory_limit_bytes');
+      //const oomkill = checkForOomkill(memUsage, memLimit);
+      const oomkill = true;
 
-    if(oomkill && !dbController.checkIfAlertAlreadyExists({pod: podsList[i].pod, issue: oomkillIssue})){
-        //create an alert
-        createAlert(podsList[i].node, podsList[i].pod, oomkillIssue, memUsage, memLimit, memUsageQuery);
-    }
+      if(oomkill && !dbController.checkIfAlertAlreadyExists({pod: podsList[i].pod, issue: oomkillIssue})){
+          //create an alert
+          createAlert(podsList[i].node, podsList[i].pod, oomkillIssue, memUsage, memLimit, memUsageQuery);
+      }
+  }
+}
+startHeartbeat();
 
 
-};
 
-
-//check request vs limit for overallocated resources if we want to do Over allocated resources
 
